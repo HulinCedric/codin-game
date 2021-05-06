bb15a32f741e7f11a1aac47a0e134a3d1261d479
Cédric Hulin <hulin.cedric@gmail.com>
2021-05-06 19:19:35 +0200

    feat: Sprint Challenge 2021 - Take Starter Kit

--- src/Contests/SpringChallenge2021/Player.cs
+++ src/Contests/SpringChallenge2021/Player.cs
@@ -1,16 +1,124 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Contests.SpringChallenge2021
 {
-    /**
-     * Auto-generated code below aims at helping you parse
-     * the standard input according to the problem statement.
-     */
+    internal class Cell
+    {
+        public int index;
+        public int[] neighbours;
+        public int richess;
+
+        public Cell(int index, int richess, int[] neighbours)
+        {
+            this.index = index;
+            this.richess = richess;
+            this.neighbours = neighbours;
+        }
+    }
+
+    internal class Tree
+    {
+        public int cellIndex;
+        public bool isDormant;
+        public bool isMine;
+        public int size;
+
+        public Tree(int cellIndex, int size, bool isMine, bool isDormant)
+        {
+            this.cellIndex = cellIndex;
+            this.size = size;
+            this.isMine = isMine;
+            this.isDormant = isDormant;
+        }
+    }
+
+    internal class Action
+    {
+        public const string WAIT = "WAIT";
+        public const string SEED = "SEED";
+        public const string GROW = "GROW";
+        public const string COMPLETE = "COMPLETE";
+        public int sourceCellIdx;
+        public int targetCellIdx;
+
+        public string type;
+
+        public Action(string type, int sourceCellIdx, int targetCellIdx)
+        {
+            this.type = type;
+            this.targetCellIdx = targetCellIdx;
+            this.sourceCellIdx = sourceCellIdx;
+        }
+
+        public Action(string type, int targetCellIdx)
+            : this(type, 0, targetCellIdx)
+        {
+        }
+
+        public Action(string type)
+            : this(type, 0, 0)
+        {
+        }
+
+        public static Action Parse(string action)
+        {
+            var parts = action.Split(" ");
+            switch (parts[0])
+            {
+                case WAIT:
+                    return new Action(WAIT);
+                case SEED:
+                    return new Action(SEED, int.Parse(parts[1]), int.Parse(parts[2]));
+                case GROW:
+                case COMPLETE:
+                default:
+                    return new Action(parts[0], int.Parse(parts[1]));
+            }
+        }
+
+        public override string ToString()
+        {
+            if (type == WAIT)
+                return WAIT;
+            if (type == SEED)
+                return string.Format("{0} {1} {2}", SEED, sourceCellIdx, targetCellIdx);
+            return string.Format("{0} {1}", type, targetCellIdx);
+        }
+    }
+
+    internal class Game
+    {
+        public List<Cell> board;
+        public int day;
+        public int myScore, opponentScore;
+        public int mySun, opponentSun;
+        public int nutrients;
+        public bool opponentIsWaiting;
+        public List<Action> possibleActions;
+        public List<Tree> trees;
+
+        public Game()
+        {
+            board = new List<Cell>();
+            possibleActions = new List<Action>();
+            trees = new List<Tree>();
+        }
+
+        public Action GetNextAction()
+            // TODO: write your algorithm here
+            => possibleActions.First();
+    }
+
     internal class Player
     {
         private static void Main(string[] args)
         {
             string[] inputs;
+
+            var game = new Game();
+
             var numberOfCells = int.Parse(Console.ReadLine()); // 37
             for (var i = 0; i < numberOfCells; i++)
             {
@@ -23,20 +131,28 @@ namespace Contests.SpringChallenge2021
                 var neigh3 = int.Parse(inputs[5]);
                 var neigh4 = int.Parse(inputs[6]);
                 var neigh5 = int.Parse(inputs[7]);
+                int[] neighs =
+                {
+                    neigh0, neigh1, neigh2, neigh3, neigh4, neigh5
+                };
+                var cell = new Cell(index, richness, neighs);
+                game.board.Add(cell);
             }
 
             // game loop
             while (true)
             {
-                var day = int.Parse(Console.ReadLine()); // the game lasts 24 days: 0-23
-                var nutrients = int.Parse(Console.ReadLine()); // the base score you gain from the next COMPLETE action
+                game.day = int.Parse(Console.ReadLine()); // the game lasts 24 days: 0-23
+                game.nutrients = int.Parse(Console.ReadLine()); // the base score you gain from the next COMPLETE action
                 inputs = Console.ReadLine().Split(' ');
-                var sun = int.Parse(inputs[0]); // your sun points
-                var score = int.Parse(inputs[1]); // your current score
+                game.mySun = int.Parse(inputs[0]); // your sun points
+                game.myScore = int.Parse(inputs[1]); // your current score
                 inputs = Console.ReadLine().Split(' ');
-                var oppSun = int.Parse(inputs[0]); // opponent's sun points
-                var oppScore = int.Parse(inputs[1]); // opponent's score
-                var oppIsWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
+                game.opponentSun = int.Parse(inputs[0]); // opponent's sun points
+                game.opponentScore = int.Parse(inputs[1]); // opponent's score
+                game.opponentIsWaiting = inputs[2] != "0"; // whether your opponent is asleep until the next day
+
+                game.trees.Clear();
                 var numberOfTrees = int.Parse(Console.ReadLine()); // the current amount of trees
                 for (var i = 0; i < numberOfTrees; i++)
                 {
@@ -45,20 +161,20 @@ namespace Contests.SpringChallenge2021
                     var size = int.Parse(inputs[1]); // size of this tree: 0-3
                     var isMine = inputs[2] != "0"; // 1 if this is your tree
                     var isDormant = inputs[3] != "0"; // 1 if this tree is dormant
+                    var tree = new Tree(cellIndex, size, isMine, isDormant);
+                    game.trees.Add(tree);
                 }
 
-                var numberOfPossibleActions = int.Parse(Console.ReadLine()); // all legal actions
-                for (var i = 0; i < numberOfPossibleActions; i++)
+                game.possibleActions.Clear();
+                var numberOfPossibleMoves = int.Parse(Console.ReadLine());
+                for (var i = 0; i < numberOfPossibleMoves; i++)
                 {
-                    var possibleAction = Console.ReadLine(); // try printing something from here to start with
+                    var possibleMove = Console.ReadLine();
+                    game.possibleActions.Add(Action.Parse(possibleMove));
                 }
 
-                // Write an action using Console.WriteLine()
-                // To debug: Console.Error.WriteLine("Debug messages...");
-
-
-                // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
-                Console.WriteLine("WAIT");
+                var action = game.GetNextAction();
+                Console.WriteLine(action);
             }
         }
     }
