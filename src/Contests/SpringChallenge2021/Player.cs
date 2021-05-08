e6731965e82ca1b13717e28244490626b7657159
Cédric Hulin <hulin.cedric@gmail.com>
2021-05-08 17:51:00 +0200

    feat: Sprint Challenge 2021 - Test central richness position strategy

--- src/Contests/SpringChallenge2021/Player.cs
+++ src/Contests/SpringChallenge2021/Player.cs
@@ -107,10 +107,64 @@ namespace CondinGame.Contests.SpringChallenge2021
 
         public Action GetNextAction()
         {
+            #region Take Central positon
+
             // TODO Strategy => Place the trees at the richness cells and take position.
+            var possibleSeedActions = possibleActions.Where(action => action.type == Action.SEED).ToList();
+            if (possibleSeedActions.Any())
+            {
+                var richnessSeedCellsIndexes =
+                    (from boardCell in board
+                        join mineTreeCellIndex in possibleSeedActions.Select(a => a.targetCellIdx) on boardCell.index
+                            equals
+                            mineTreeCellIndex
+                        where boardCell.richess == 3
+                        select boardCell.index).ToList();
+
+                if (richnessSeedCellsIndexes.Any())
+                    // TODO Compute the best candidate action depending sun cost  
+                    return possibleSeedActions
+                        .Last(a => a.targetCellIdx == richnessSeedCellsIndexes.Last());
+            }
             
+            var possibleGrowActions = possibleActions.Where(action => action.type == Action.GROW).ToList();
+            if (possibleGrowActions.Any())
+            {
+                var richnessGrowCellsIndexes =
+                    (from boardCell in board
+                        join mineTreeCellIndex in possibleGrowActions.Select(a => a.targetCellIdx) on boardCell.index
+                            equals
+                            mineTreeCellIndex
+                        where boardCell.richess == 3
+                        select boardCell.index).ToList();
+
+                if (richnessGrowCellsIndexes.Any())
+                    // TODO Compute the best candidate action depending sun cost  
+                    return possibleGrowActions
+                        .Last(a => a.targetCellIdx == richnessGrowCellsIndexes.Last());
+            }
+            
+            if (possibleGrowActions.Any())
+            {
+                var richnessGrowCellsIndexes =
+                    (from boardCell in board
+                        join mineTreeCellIndex in possibleGrowActions.Select(a => a.targetCellIdx) on boardCell.index
+                            equals
+                            mineTreeCellIndex
+                        where boardCell.richess == 2
+                        select boardCell.index).ToList();
+
+                if (richnessGrowCellsIndexes.Any())
+                    // TODO Compute the best candidate action depending sun cost  
+                    return possibleGrowActions
+                        .Last(a => a.targetCellIdx == richnessGrowCellsIndexes.Last());
+            }
+
+            #endregion
+
+
             // TODO Compute action cost in sun point
-            
+
             if (mySun >= SunPointsRequiredToCompleteTreeLifecycle)
             {
                 var collectableTrees = GetCollectableTreeCellIndexes().ToList();
@@ -129,7 +183,6 @@ namespace CondinGame.Contests.SpringChallenge2021
                 return new Action(Action.GROW, treeOrderedBySizeAndByRichness.Last());
             }
 
-            var possibleSeedActions = possibleActions.Where(action => action.type == Action.SEED).ToList();
             if (possibleSeedActions.Any())
             {
                 var seedCellsIndexesOrderByRichness =
@@ -224,6 +277,7 @@ namespace CondinGame.Contests.SpringChallenge2021
                 for (var i = 0; i < numberOfPossibleMoves; i++)
                 {
                     var possibleMove = Console.ReadLine();
+                    Console.Error.WriteLine(possibleMove);
                     game.possibleActions.Add(Action.Parse(possibleMove));
                 }
 
